629d4160d5be633eb649b0b74f3421d380cedf17
Stephen Brown <stephen.jr36@gmail.com>
2024-10-08 16:24:16 -0600

    finished listing initial routes

--- backend/server/server.js
+++ backend/server/server.js
@@ -29,15 +29,21 @@ app.post('/api/register');
 // TODO: import recipes functions
 
 // themealdb api request for recipe search
-app.get('/api/recipe-search/:search');
+app.get('/api/recipe-search/:userSearchStr');  // what is a better name for the user input string???
 // get user's saved recipes
 app.get('/api/user-recipes');
 // user saves a recipe
 app.get('/api/save-recipe');
 // user un-saves a recipe
-app.get('/api/unsave-recipe');
+app.delete('/api/unsave-recipe');
 // display recipe modal
 app/get('/app/recipe-detail/:recipeId');
+// rating for recipe
+app.get('/api/rate-recipe');
+// add week
+app.get('/api/add-week')
+// delete week
+app.delete('/api/delete-week/:weekId')
 
 // Planner Endpoints
 // TODO: import planner functions
@@ -45,13 +51,19 @@ app/get('/app/recipe-detail/:recipeId');
 // get user week planner data
 app.get('/api/user-weeks');
 // user adds recipe to week
-// user removes recipe from week?
+app.post('/api/add-recipe/') // needs weekId and dayId and recipeId
+// user moves recipe from one day to another day or to another week?
+app.put('/api/week-meal-edit/') // needs weekId and dayId
+// user removes recipe from planner
+app.delete('/api/remove-week-meal/:weekMealId');
 
 // Grocery List Endpoints
 // TODO: import grocery list functions
 
-// generate grocery list (should this be on the front end)
-// save grocery list to db
+// generate grocery list
+app.get('/api/grocery-list-data');
+// save grocery list to db (stretch goal)
+// app.get('/api/save-grocery-list'); // stretch goal
 
 ViteExpress.listen(app, port, () => {
   console.log(`Server running on http://localhost:${port}`);
