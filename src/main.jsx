0bf82bdc777c02e52f453cff2c3ed4896b49d5d8
Lauryn Thayne <lalder57@gmail.com>
2024-10-08 12:33:34 -0600

    Started making changes to main.jsx, but I need to set up redux before continuing.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { React, StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+// import store from "./redux/store.js";
+import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+const Router = () => {
+  // Session Check stuff we will implement once we have Redux set up:
+
+  // const userId = useSelector((state) => state.userId);
+  // const dispatch = useDispatch();
+
+  // const sessionCheck = async () => {
+  //   const res = await axios.get("/api/session-check");
+
+  //   if (res.data.success) {
+  //     dispatch({
+  //       type: "USER_AUTH",
+  //       payload: res.data.userId,
+  //     });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   sessionCheck();
+  // }, [userId]);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  );
+  return <RouterProvider router={router} />;
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </StrictMode>
+);
