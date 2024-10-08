84387de9567c6d649ea0346efbc9fa4e203b157d
Lauryn Thayne <lalder57@gmail.com>
2024-10-08 12:53:25 -0600

    Added redux folder with store.js and reducer.js. Main.jsx now rendering.

---
+++ src/redux/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  userId: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // frontend components will dispatch an action object:
+    // { type: "USER-AUTH", payload: { userId: userId, } }
+    case "USER_AUTH":
+      return {
+        ...state,
+        userId: action.payload.userId,
+      };
+
+    // triggered from front end with this dispatch action object:
+    // { type: "LOGOUT"}
+    case "LOGOUT":
+      return {
+        ...state,
+        userId: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
