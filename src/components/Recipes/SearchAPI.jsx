704d0743d209fd4b8e5a92a7ceae51fea6d42bfa
Lauryn Thayne <lalder57@gmail.com>
2024-10-09 12:43:50 -0600

    Rough outline of SearchAPI component on RecipesPage. Will finalize with backend

--- src/components/Recipes/SearchAPI.jsx
+++ src/components/Recipes/SearchAPI.jsx
@@ -1,5 +1,41 @@
-const SearchAPI = () => {
-  return <div></div>;
+import { useState } from "react";
+
+const SearchAPI = ({ setRecipeData }) => {
+  // set state values
+  const [searchInput, setSearchInput] = useState("");
+
+  // create function to handle search
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    // create body object
+    const searchValue = {
+      searchInput,
+    };
+
+    // make call to backend endpoint
+    const res = await axios.get("/api/searchAPI", searchValue);
+
+    // if successful -- setRecipeData will = the new recipeData from the axios call, if not -- display message
+    if (res.data.success) {
+      setRecipeData(res.data.searchResults);
+    } else {
+      return <p>{res.data.message}</p>;
+    }
+  };
+
+  return (
+    <form>
+      <label htmlFor="search"></label>
+      <input
+        type="text"
+        placeholder="Search recipes"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 };
 
 export default SearchAPI;
