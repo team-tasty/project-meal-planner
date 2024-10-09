4445955ef53caa3b02a161efb0502445f8069042
Lauryn Thayne <lalder57@gmail.com>
2024-10-09 15:44:04 -0600

    Worked on making the recipeCard component. Got the map working with mock data. Did the same thing for userRecipes page, implemented a search feature for user's saved recipes

--- src/components/Planner/PlannerPage.jsx
+++ src/components/Planner/PlannerPage.jsx
@@ -1,5 +1,13 @@
+import WeeklyPlannerBox from "./WeeklyPlannerBox/WeeklyPlannerBox.jsx";
+import UserRecipes from "./UserRecipes/UserRecipes.jsx";
+
 const PlannerPage = () => {
-  return <div>Weekly Planner</div>;
+  return (
+    <div>
+      Weekly Planner
+      <UserRecipes />
+    </div>
+  );
 };
 
 export default PlannerPage;
--- src/components/Planner/UserRecipes/UserRecipes.jsx
+++ src/components/Planner/UserRecipes/UserRecipes.jsx
@@ -1,5 +1,29 @@
+import SearchRecipes from "./SearchRecipes.jsx";
+import RecipeCard from "../../Recipes/RecipeCard.jsx";
+import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+
 const UserRecipes = () => {
-  return <div></div>;
+  // get userSavedRecipes from a loader function
+  // const { userRecipes } = useLoaderData();
+  const [recipesData, setRecipesData] = useState([
+    {
+      recipeId: 1,
+      title: "Chicken Alfredo",
+      image:
+        "https://borrowedbites.com/wp-content/uploads/2024/01/Square-One-Pot-Chicken-Alfredo.jpg",
+      category: "Pasta",
+    },
+    {
+      recipeId: 2,
+      title: "Brownies",
+      image:
+        "https://mytxkitchen.com/wp-content/uploads/2022/08/Fudgy-Chocolate-Brownies-4-500x375.jpg",
+      category: "Desserts",
+    },
+  ]);
+
+  return <SearchRecipes recipesData={recipesData} />;
 };
 
 export default UserRecipes;
