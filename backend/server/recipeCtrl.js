ecb2c65aaa3a167b857169e7426ee15129c8404d
Stephen Brown <stephen.jr36@gmail.com>
2024-10-10 11:27:27 -0600

    recipeSearch fn returns recipeData array in response when success is true

--- backend/server/recipeCtrl.js
+++ backend/server/recipeCtrl.js
@@ -1,31 +1,74 @@
 import axios from "axios";
 import { User } from "../db/model.js";
+import convertIngredient from "../../functions/parseIngredient.js";
 
 export const recipeFns = {
   recipeSearch: async (req, res) => {
-    console.log();
-    console.log('request received')
-    console.log();
-
     const userId = req.session.userId;
 
-    // TODO: uncomment once fn is complete
-    // if (!userId) {
-    //   return res.send({
-    //     message: 'No user in session',
-    //     success: false
-    //   });
-    // };
+    if (!userId) {
+      return res.send({
+        message: 'No user in session',
+        success: false
+      });
+    };
 
     const { searchInput } = req.body;
 
     const searchRes = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
 
-    console.log(`themealdb API response:`, searchRes.data)
+    if (!searchRes.data) {
+      return res.send({
+        message: 'Could not get data from external API',
+        success: false
+      });
+    };
 
-    res.send({
-      message: 'request was successful',
-      recipeData: searchRes.data
+    const recipeData = [];
+    
+    try {
+      for (const meal of searchRes.data.meals) {
+        let recipeObj = {};
+        
+        recipeObj.recipeId = meal.idMeal; // should this be named something other than 'recipeId' to avoid conflicts if user recipes and external recipes are displayed at the same time?
+        recipeObj.title = meal.strMeal;
+        recipeObj.instruction = meal.strInstructions;
+        recipeObj.image = meal.strMealThumb;
+        recipeObj.category = meal.strCategory;
+        recipeObj.area = meal.strArea;
+        recipeObj.tag = meal.strTags;
+        recipeObj.recipeIngredients = [];
+  
+        // Recipe Ingredients loop
+        for (let i = 1; i <= 20; i++) {
+          const recipeIngredientObj = {};
+  
+          const qtyUnitArr = convertIngredient(meal[`strMeasure${i}`]);
+  
+          recipeIngredientObj.measurementQuantity = { quantity: qtyUnitArr[0] };
+          recipeIngredientObj.measurementUnit = { unit: qtyUnitArr[1] };
+          recipeIngredientObj.ingredient = { ingredient: meal[`strIngredient${i}`].toLowerCase() };
+  
+          recipeObj.recipeIngredients.push(recipeIngredientObj);
+        };
+  
+        recipeData.push(recipeObj);
+      };
+    } catch(error) {
+      console.log();
+      console.error(error);
+      console.log();
+
+      return res.send({
+        message: 'Could not parse and manipulate data from external API',
+        success: false
+      });
+    };
+
+    return res.send({
+      message: 'Successfully retreived, parsed, and manipulated data from external API',
+      success: true,
+      recipeData: recipeData,
     });
   }
-};
+}
