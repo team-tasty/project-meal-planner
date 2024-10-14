738434aea9dfb8c29959c4a430813c1c3bf337fc
Stephen Brown <stephen.jr36@gmail.com>
2024-10-14 12:19:43 -0600

    created initial plannerFns object to import planner controller fns to server.js

---
+++ backend/server/plannerCtrl.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { User, Week, Day, Recipe, WeekMeal } from "../db/model";
+
+export default plannerFns = {
+  userWeeks: async (req, res) => {
+
+  },
+  
+  addUserWeek: async (req, res) => {
+
+  },
+  
+  deleteUserWeek: async (req, res) => {
+
+  },
+
+  days: async (req, res) => {
+
+  },
+
+  createWeekMeal: async (req, res) => {
+
+  },
+
+  editWeekMeal: async (req, res) => {
+
+  },
+  
+  deleteWeekMeal: async (req, res) => {
+
+  },
+}
