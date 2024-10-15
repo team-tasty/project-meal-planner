eeafa19b2d59b42e14259198f12701c9cafe7ab3
Lauryn Thayne <lalder57@gmail.com>
2024-10-15 12:55:40 -0600

    Trying to get form to add new input fields for ingredients but add button is clicked.

--- src/components/Recipes/AddRecipeForm.jsx
+++ src/components/Recipes/AddRecipeForm.jsx
@@ -8,9 +8,49 @@ const AddRecipeForm = () => {
   const [tag, setTag] = useState("");
   const [area, setArea] = useState("");
   const [instruction, setInstruction] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [unit, setUnit] = useState([]);
+  const [name, setName] = useState("");
+  const [isAddingNewIngredient, setIsAddingNewIngredient] = useState(false);
 
   const handleAddRecipe = () => {
     // make backend call to add a user recipe to the db and save it to user
+  };
+
+  const handleAddIngredient = () => {
+    setIsAddingNewIngredient(true);
+    const handleDeleteIngredient = () => {};
+    return (
+      <div>
+        <label htmlFor="quantity">Quantity</label>
+        <input
+          value={quantity}
+          type="number"
+          placeholder="2"
+          required
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <label htmlFor="unit">Unit</label>
+        <input
+          value={unit}
+          type="text"
+          pattern="text"
+          placeholder="cups"
+          required
+          onChange={(e) => setUnit(e.target.value)}
+        />
+        <label htmlFor="name">Name</label>
+        <input
+          value={name}
+          type="text"
+          placeholder="flour"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <button onClick={handleDeleteIngredient}>Trash</button>
+      </div>
+    );
   };
   return (
     <div>
@@ -20,7 +60,7 @@ const AddRecipeForm = () => {
         <input
           value={title}
           type="text"
-          placeholder="Recipe Title"
+          placeholder="Chocolate chip cookies"
           required
           onChange={(e) => setTitle(e.target.value)}
         />
@@ -28,10 +68,46 @@ const AddRecipeForm = () => {
         <input
           value={image}
           type="text"
-          placeholder="Recipe Image"
+          placeholder="https://www.glorioustreats.com.jpg"
           required
           onChange={(e) => setImage(e.target.value)}
         />
+        <label htmlFor="area">Area</label>
+        <input
+          value={area}
+          type="text"
+          placeholder="American"
+          required
+          onChange={(e) => setArea(e.target.value)}
+        />
+        <label htmlFor="category">Category</label>
+        <input
+          value={category}
+          type="text"
+          placeholder="Dessert"
+          required
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <label htmlFor="tag">Tags</label>
+        <input
+          value={tag}
+          type="text"
+          placeholder="e.g. baking,chocolate,gooey"
+          required
+          onChange={(e) => setTag(e.target.value)}
+        />
+        <label htmlFor="instruction">Cooking Instructions</label>
+        <textarea
+          value={instruction}
+          type="text"
+          placeholder="e.g. Preheat the oven to..."
+          required
+          onChange={(e) => setInstruction(e.target.value)}
+          className="border border-black p-2"
+        />
+        <button onClick={handleAddIngredient} type="button">
+          Add Ingredient
+        </button>
       </form>
     </div>
   );
