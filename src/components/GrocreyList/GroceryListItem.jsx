011a80b66311d49865736478fcef450370a92bd8
Lauryn Thayne <lalder57@gmail.com>
2024-10-18 12:37:33 -0600

    full grocery list button working with the backend. Set up a new value in redux.

--- src/components/GrocreyList/GroceryListItem.jsx
+++ src/components/GrocreyList/GroceryListItem.jsx
@@ -1,160 +1,169 @@
 import groceryList from "../../../functions/groceryList.js";
 import groceryListPracticeData from "../../../functions/groceryListPracticeData.js";
 import userWeeksExampleRes2 from "../../../functions/userWeeksExampleRes2.js";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const GroceryListItem = () => {
+  const groceryList = useSelector((state) => state.groceryList);
+  console.log(groceryList);
 
-  const testData = groceryList(userWeeksExampleRes2)
+  const testData = groceryList;
 
-  const cannedList = testData.filter((arrIng) => arrIng.category === "canned")
-  .map((arrIng, index) => {
-    return (
-      <li key={`canned${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const cannedList = testData
+    .filter((arrIng) => arrIng.category === "canned")
+    .map((arrIng, index) => {
+      return (
+        <li key={`canned${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const frozenList = testData.filter((arrIng) => arrIng.category === "frozen")
-  .map((arrIng, index) => {
-    return (
-      <li key={`frozen${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const frozenList = testData
+    .filter((arrIng) => arrIng.category === "frozen")
+    .map((arrIng, index) => {
+      return (
+        <li key={`frozen${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const processedList = testData.filter((arrIng) => arrIng.category === "processed")
-  .map((arrIng, index) => {
-    return (
-      <li key={`processed${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const processedList = testData
+    .filter((arrIng) => arrIng.category === "processed")
+    .map((arrIng, index) => {
+      return (
+        <li key={`processed${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const bakingList = testData.filter((arrIng) => arrIng.category === "baking")
-  .map((arrIng, index) => {
-    return (
-      <li key={`baking${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const bakingList = testData
+    .filter((arrIng) => arrIng.category === "baking")
+    .map((arrIng, index) => {
+      return (
+        <li key={`baking${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const produceList = testData.filter((arrIng) => arrIng.category === "produce")
-  .map((arrIng, index) => {
-    return (
-      <li key={`produce${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const produceList = testData
+    .filter((arrIng) => arrIng.category === "produce")
+    .map((arrIng, index) => {
+      return (
+        <li key={`produce${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const dairyList = testData.filter((arrIng) => arrIng.category === "dairy")
-  .map((arrIng, index) => {
-    return (
-      <li key={`dairy${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const dairyList = testData
+    .filter((arrIng) => arrIng.category === "dairy")
+    .map((arrIng, index) => {
+      return (
+        <li key={`dairy${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const proteinList = testData.filter((arrIng) => arrIng.category === "protein")
-  .map((arrIng, index) => {
-    return (
-      <li key={`protein${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const proteinList = testData
+    .filter((arrIng) => arrIng.category === "protein")
+    .map((arrIng, index) => {
+      return (
+        <li key={`protein${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
 
-  const otherList = testData.filter((arrIng) => arrIng.category === "other")
-  .map((arrIng, index) => {
-    return (
-      <li key={`other${index}`}>
-        <input type="checkbox" className="ms-2 me-3" />
-        {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}{arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
-      </li>
-    )
-  })
+  const otherList = testData
+    .filter((arrIng) => arrIng.category === "other")
+    .map((arrIng, index) => {
+      return (
+        <li key={`other${index}`}>
+          <input type="checkbox" className="ms-2 me-3" />
+          {arrIng.quantity > 0 ? `${arrIng.quantity} ` : ""}
+          {arrIng.unit !== "null" ? arrIng.unit : ""} {arrIng.ingredient}
+        </li>
+      );
+    });
+
+  // useEffect(() => {
+
+  // }, [groceryList])
 
   return (
-  <>
-    <h1 className="underline">Grocery List</h1>
-    { cannedList.length > 0 &&
-      <>
-      <h2 className="underline">Canned</h2>
-        <ul>
-          {cannedList}
-        </ul>
-      </>
-    }
-    { frozenList.length > 0 &&
-      <>
-      <h2 className="underline">Frozen</h2>
-        <ul>
-          {frozenList}
-        </ul>
-      </>
-    }
-    { processedList.length > 0 &&
-      <>
-      <h2 className="underline">Processed</h2>
-        <ul>
-          {processedList}
-        </ul>
-      </>
-    }
-    { bakingList.length > 0 &&
-      <>
-      <h2 className="underline">Baking</h2>
-        <ul>
-          {bakingList}
-        </ul>
-      </>
-    }
-    { produceList.length > 0 &&
-      <>
-      <h2 className="underline">Produce</h2>
-        <ul>
-          {produceList}
-        </ul>
-      </>
-    }
-    { dairyList.length > 0 &&
-      <>
-      <h2 className="underline">Dairy</h2>
-        <ul>
-          {dairyList}
-        </ul>
-      </>
-    }
-    { proteinList.length > 0 &&
-      <>
-      <h2 className="underline">Protein</h2>
-        <ul>
-          {proteinList}
-        </ul>
-      </>
-    }
-    { otherList.length > 0 &&
-      <>
-      <h2 className="underline">Other</h2>
-        <ul>
-          {otherList}
-        </ul>
-      </>
-    }
-  </>
-  )
+    <>
+      <h1 className="underline">Grocery List</h1>
+      {cannedList.length > 0 && (
+        <>
+          <h2 className="underline">Canned</h2>
+          <ul>{cannedList}</ul>
+        </>
+      )}
+      {frozenList.length > 0 && (
+        <>
+          <h2 className="underline">Frozen</h2>
+          <ul>{frozenList}</ul>
+        </>
+      )}
+      {processedList.length > 0 && (
+        <>
+          <h2 className="underline">Processed</h2>
+          <ul>{processedList}</ul>
+        </>
+      )}
+      {bakingList.length > 0 && (
+        <>
+          <h2 className="underline">Baking</h2>
+          <ul>{bakingList}</ul>
+        </>
+      )}
+      {produceList.length > 0 && (
+        <>
+          <h2 className="underline">Produce</h2>
+          <ul>{produceList}</ul>
+        </>
+      )}
+      {dairyList.length > 0 && (
+        <>
+          <h2 className="underline">Dairy</h2>
+          <ul>{dairyList}</ul>
+        </>
+      )}
+      {proteinList.length > 0 && (
+        <>
+          <h2 className="underline">Protein</h2>
+          <ul>{proteinList}</ul>
+        </>
+      )}
+      {otherList.length > 0 && (
+        <>
+          <h2 className="underline">Other</h2>
+          <ul>{otherList}</ul>
+        </>
+      )}
+    </>
+  );
 };
 
 export default GroceryListItem;
--- src/components/Planner/WeeklyPlannerBox/FullGroceryListButton.jsx
+++ src/components/Planner/WeeklyPlannerBox/FullGroceryListButton.jsx
@@ -1,14 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import { useSelector, useDispatch } from "react-redux";
 const FullGroceryListButton = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleGeneratelist = async () => {
     // make axios call to backend to multiple weeks grocery list endpoint
     const res = await axios.get("/api/grocery-list");
-
-    navigate("/app/groceryList");
+    console.log(res.data);
+    if (res.data.success) {
+      dispatch({
+        type: "UPDATE_GROCERY_LIST",
+        payload: res.data.groceryList,
+      });
+      navigate("/app/groceryList");
+    }
   };
   return (
     <div>
