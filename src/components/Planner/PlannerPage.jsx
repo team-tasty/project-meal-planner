19f3457813d81f6aa5b88cacdf42e563b6de65e0
Lauryn Thayne <lalder57@gmail.com>
2024-10-22 13:48:55 -0600

    styling done to the planner page with drag and drop still working

--- src/components/Planner/PlannerPage.jsx
+++ src/components/Planner/PlannerPage.jsx
@@ -1,7 +1,7 @@
 import WeeklyPlannerBox from "./WeeklyPlannerBox/WeeklyPlannerBox.jsx";
 import UserRecipes from "./UserRecipes/UserRecipes.jsx";
 import { DragDropContext } from "@hello-pangea/dnd";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import AddWeeklyPlannerButton from "./WeeklyPlannerBox/AddWeeklyPlannerButton.jsx";
@@ -12,20 +12,9 @@ const PlannerPage = () => {
   const [userRecipes, setUserRecipes] = useState(useLoaderData().userRecipes);
   const [userWeeks, setUserWeeks] = useState(useLoaderData().userWeeks);
   const daysData = useLoaderData().daysData;
-  console.log("USER WEEKS:", userWeeks);
-
-  console.log(userWeeks[0].weekMeals); // one week's planned meals
-
-  // const initialPlannedRecipes = [[], [], [], [], [], [], []];
-
-  // // const [userRecipes] = useState(initialUserRecipes);
-  // const [plannedRecipes, setPlannedRecipes] = useState(initialPlannedRecipes);
 
   const handleOnDragEnd = async (result) => {
     const { source, destination } = result;
-    console.log("SOURCE", source);
-    console.log("DESTINATION", destination);
-    console.log("RESULT", result);
 
     // if dropped outside of any droppable, do nothing
     if (!destination) return;
@@ -48,7 +37,6 @@ const PlannerPage = () => {
     const dayArrayLength = userWeeks
       .filter((week) => week.weekId === weekId)[0]
       .weekMeals.filter((weekMeals) => weekMeals.dayId === dayId).length;
-    console.log("********", dayArrayLength);
 
     // set request to update the db
     // make a call to add a weakMeal entry (will need recipeId, weekId, dayId in the body obj)
@@ -60,7 +48,6 @@ const PlannerPage = () => {
     // logic to prevent adding more that 3 recipes to a day
     if (dayArrayLength < 3) {
       const res = await axios.post("/api/add-week-meal/", bodyObj);
-      console.log(res.data);
 
       if (res.data.success) {
         setUserWeeks(res.data.userWeeks);
@@ -70,12 +57,9 @@ const PlannerPage = () => {
 
   // handle a delettion from list 2 (plannedRecipes)
   const handleDelete = async (weekMealId) => {
-    // const newPlannedRecipes = [...plannedRecipes];
-    // newPlannedRecipes[dayIndex].splice(recipeIndex, 1);
     // need a calla to the backend to update the db?
     // make a call to remove recipe from the weakMeals (will need weakMealId)
     const res = await axios.delete(`/api/delete-week-meal/${weekMealId}`);
-    console.log(res.data);
     if (res.data.success) {
       // if successfull setUserWeeks to res.data.updatedUserWeeks
       setUserWeeks(res.data.userWeeks);
@@ -83,12 +67,13 @@ const PlannerPage = () => {
   };
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
-      <div>
+      <div className="">
         <div className="flex flex-col justify-around items-center h-[90vh] box-border lg:justify-around lg:flex lg:flex-row-reverse lg:items-start">
           <UserRecipes
             userRecipes={userRecipes}
             setUserRecipes={setUserRecipes}
           />
+
           <WeeklyPlannerBox
             handleDelete={handleDelete}
             userWeeks={userWeeks}
@@ -96,7 +81,8 @@ const PlannerPage = () => {
             daysData={daysData}
             handleOnDragEnd={handleOnDragEnd}
           />
-          <div className="flex justify-center items-center gap-4 h-[5vh] lg:h-[7vh] lg:fixed lg:bottom-0">
+
+          <div className="flex justify-center items-center gap-4 h-[5vh] lg:fixed lg:bottom-0">
             <AddWeeklyPlannerButton
               setUserWeeks={setUserWeeks}
               userWeeks={userWeeks}
@@ -106,36 +92,6 @@ const PlannerPage = () => {
         </div>
       </div>
     </DragDropContext>
-    //     <div className="hidden lg:flex lg:flex-col">
-    //       <div className="time-picker-scrollbar flex flex-col h-[50vh] overflow-auto border my-3 lg:h-[80vh] lg:w-[40vw] lg:items-center lg:mt-6 lg:my-0 border-black rounded-md">
-    //         {/* <WeeklyPlannerBox
-    //           handleDelete={handleDelete}
-    //           userWeeks={userWeeks}
-    //           setUserWeeks={setUserWeeks}
-    //           daysData={daysData}
-    //           handleOnDragEnd={handleOnDragEnd}
-    //         /> */}
-    //       </div>
-
-    //     </div>
-    //     <div className="flex flex-col h-[50vh] overflow-auto lg:hidden border my-3 lg:h-[80vh] lg:w-[40vw] lg:items-center lg:mt-6 lg:my-0 border-black rounded-md">
-    //       <WeeklyPlannerBox
-    //         handleDelete={handleDelete}
-    //         userWeeks={userWeeks}
-    //         setUserWeeks={setUserWeeks}
-    //         daysData={daysData}
-    //         handleOnDragEnd={handleOnDragEnd}
-    //       />
-    //     </div>
-    //     <div className="flex justify-center lg:hidden items-center gap-4 h-[5vh] lg:h-[7vh]">
-    //       <AddWeeklyPlannerButton
-    //         setUserWeeks={setUserWeeks}
-    //         userWeeks={userWeeks}
-    //       />
-    //       <FullGroceryListButton />
-    //     </div>
-    //   </div>
-    // </DragDropContext>
   );
 };
 
